117c9dee1cc20ffb639ff6bfdeb2743a1fff9b3c
hyae4924 <hyae4924@gmail.com>
2023-01-06 04:25:47 +0900

    [FEAT] Add version 1.0.0

--- main.js
+++ main.js
@@ -6,19 +6,24 @@ const projects = document.querySelector(".projects");
 const contact = document.querySelector(".contact");
 const skill__btn = document.querySelectorAll(".skill__btn");
 const navbar__meun__item = document.querySelectorAll(".navbar__menu__item");
+const contact_me_button = document.querySelector(".home__btn");
 
-//It help to know user clickPoint
-window.addEventListener("scroll", () => {
-  for (const key in skill__btn) {
-    if (isNaN(key)) continue;
-    if (skill__btn[key].className === "skill__btn") {
-      skill__btn[key].classList.replace("skill__btn", "active");
-    }
-    setTimeout(() => {
-      skill__btn[key].classList.replace("active", "skill__btn");
-    }, "200");
-  }
+contact_me_button.addEventListener("click", e => {
+  contact.scrollIntoView({ behavior: "smooth" });
 });
+
+// It help to know user clickPoint
+// window.addEventListener("scroll", () => {
+//   for (const key in skill__btn) {
+//     if (isNaN(key)) continue;
+//     if (skill__btn[key].className === "skill__btn") {
+//       skill__btn[key].classList.replace("skill__btn", "active");
+//     }
+//     setTimeout(() => {
+//       skill__btn[key].classList.replace("active", "skill__btn");
+//     }, "400");
+//   }
+// });
 
 // It make to go selected point (Navbar)
 for (const key in navbar__meun__item) {
@@ -33,11 +38,38 @@ for (const key in navbar__meun__item) {
       element = about;
     } else if (text === "Skills") {
       element = skills;
-    } else if (text === "MyProject") {
+    } else if (text === "Projects") {
       element = projects;
     } else if (text === "Contact") {
       element = contact;
     }
     element.scrollIntoView({ behavior: "smooth" });
   });
+}
+
+const skill_item = document.querySelectorAll(".skill__item");
+
+const open = e => {
+  const { target } = e;
+  let result;
+  if (target.className.includes("skill__item")) {
+    result = target;
+    console.log(1);
+  } else if (target.parentElement.className.includes("skill__item")) {
+    result = target.parentElement;
+    console.log(2);
+  } else if (
+    target.parentElement.parentElement.className.includes("skill__item")
+  ) {
+    result = target.parentElement.parentElement;
+    console.log(3);
+  }
+  if (result.classList.length === 1) {
+    result.className += " active2";
+  } else result.className = "skill__item";
+};
+
+for (const key in skill_item) {
+  if (isNaN(key)) continue;
+  skill_item[key].addEventListener("click", open);
 }
